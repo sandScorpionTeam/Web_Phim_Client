577718c63d5289a4050e55fd7bbcc00fde2cf31a
Doanh <doanhhnqt74@gmail.com>
2019-10-04 02:19:17 +0700

    add menu header

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -3,8 +3,46 @@ import "./header.scss"
 
 const Header = () => {
   return (
-    <header className="header-layout">
-      This is the header
+    <header className="header-layout header-theme">
+      <div className="container">
+        <div className="logo-block">
+          <a href="/">          
+            <img src="/logo192.png" alt="Film it"
+              className="logo-img" />
+            <span className="logo-text">film it</span>
+          </a>
+        </div>        
+        <nav className="primary-menu">
+          <ul className="left-menu">
+            <li>
+              <a href="/home">Home</a>
+              <span className="sub-toggle"></span>
+            </li>
+            <li>
+              <a href="/movies">Movies</a>
+              <span className="sub-toggle"></span>
+            </li>
+            <li>
+              <a href="tvshows">TV Shows</a>
+              <span className="sub-toggle"></span>
+            </li>
+            <li>
+              <a href="/celebrities">Celebrities</a>
+              <span className="sub-toggle"></span>
+            </li>
+            <li>
+              <a href="/news">News</a>
+              <span className="sub-toggle"></span>
+            </li>
+          </ul>          
+          <ul className="right-menu">
+            <li className="login-btn">
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </nav>
+        <button className="collapse-button">toogle menu</button>
+      </div>
     </header>
   )
 }
