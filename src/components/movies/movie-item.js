eba57273dbe043a155455cc5d53879d6650c60e1
Doanh <doanhhnqt74@gmail.com>
2019-10-08 07:13:53 +0700

    add movies list

--- src/components/movies/movie-item.js
+++ src/components/movies/movie-item.js
@@ -3,9 +3,18 @@ import "./movie.scss";
 
 const Movie = () => {
   return (
-    <div className="movie-item">
-      This is a movie
-    </div>
+    <a className="movie-item" title="Đốt - Burn" href="/">
+      <div class="block-wrapper">
+        <div class="movie-thumbnail">
+          <img src="http://lorempixel.com/200/300/" alt="Đốt - Burn"/>
+        </div>
+        <div class="movie-meta">
+          <h3 class="movie-title1">Đốt</h3>
+          <span class="movie-title2">Burn</span>
+          <span class="movie-duration">120 phút</span>
+        </div>
+      </div>      
+    </a>
   )
 }
 
--- src/components/movies/movies-list.js
+++ src/components/movies/movies-list.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Movie from './movie-item';
+import "./movie.scss";
 
 const Movies = () => {
-
   return (
     <div className="movies-list">
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
       <Movie />
       <Movie />
       <Movie />
