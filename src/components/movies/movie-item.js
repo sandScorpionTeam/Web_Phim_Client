df38c0d291a38c5ab61dd5108c721bfd5878b4c6
Doanh <doanhhnqt74@gmail.com>
2019-10-11 23:50:24 +0700

    grid based on screen width for movies section

--- src/components/movies/movie-item.js
+++ src/components/movies/movie-item.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import "./movie.scss";
+import clsx from 'clsx';
 
 const Movie = () => {
   return (
-    <a className="movie-item" title="Đốt - Burn" href="/">
+    <a className={clsx('movie-item',
+      'is-one-fifth-fullhd',
+      'is-one-quarter-desktop',
+      'is-one-third-tablet',
+      'is-half-mobile')}
+      title="Đốt - Burn" href="/">
       <div class="block-wrapper">
         <div class="movie-thumbnail">
           <img src="http://lorempixel.com/200/300/" alt="Đốt - Burn"/>
